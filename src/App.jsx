77cac784c6ffc4667388f00bbe0e32600d32eaaa
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-03-31 09:35:12 +0300

    Changed my components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,38 @@
      const contentNode = document.getElementById('contents');
 
-     class IssueFilter extends React.Component{
+          class IssueTable extends React.Component{
          render() {
+             const borderedStyle = {border:"1px solid silver", padding:6}
              return(
-                 <div>This is a placeholder for the Issue Filter.</div>
+                 <table style={{borderCollapse:"collapse"}}>
+                     <thead>
+                         <tr>
+                             <th style={borderedStyle}>Id</th>
+                             <th style={borderedStyle}>Title</th>
+                         </tr>
+                     </thead>
+                <tbody>
+                     <IssueRow issue_id={1} 
+                     issue_title = "Error in console when clicking Add"
+                     />
+                      <IssueRow issue_id={2} 
+                     issue_title = "Missing bottom border on panel"
+                     />
+                     </tbody>
+                 </table>
              );
          }
      }
-     class IssueTable extends React.Component{
-         render() {
+    
+     class IssueRow extends React.Component{
+         render(){
+             const borderedStyle = {border:"1px solid silver", padding:4}
              return(
-                 <div>This is a placeholder for the table of Issues.</div>
-             );
-         }
-     }
-     class IssueAdd extends React.Component{
-         render() {
-             return(
-                 <div>This is a placeholder for an Issue Add entry form.</div>
-             );
+                 <tr>
+                     <td style={borderedStyle}>{this.props.issue_id}</td>
+                     <td style={borderedStyle}>{this.props.issue_title}</td>
+                 </tr>
+             )
          }
      }
      class IssueList extends React.Component{
@@ -26,11 +40,11 @@
              return(
                  <div>
                      <h1>Issue Tracker</h1>
-                     <IssueFilter />
-                     <hr />
+                    
                      <IssueTable />
+                     <IssueRow />
                      <hr />
-                     <IssueAdd />
+                     <h1>This is a placeholder for an issue Add Entry form</h1>
                  </div>
              )
          }
